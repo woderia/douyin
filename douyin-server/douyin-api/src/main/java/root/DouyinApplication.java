311f82c3e1b226fb01d2cb876e1a5cec37cfe421
tuwq <aheadqiang@126.com>
2018-07-10 00:07:54 +0800

    jdk10+springboot打包成jar包和war包

--- douyin-server/douyin-api/src/main/java/root/DouyinApplication.java
+++ douyin-server/douyin-api/src/main/java/root/DouyinApplication.java
@@ -3,14 +3,22 @@ package root;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages="root")
 @ComponentScan(basePackages={"root","org.n3r.idworker"})
-public class DouyinApplication {
+public class DouyinApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(DouyinApplication.class, args);
-	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DouyinApplication.class);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DouyinApplication.class, args);
+    }
 }
